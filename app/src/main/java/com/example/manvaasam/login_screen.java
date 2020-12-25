b1668a5282c1b615de22a544242a179ed89aa3dd
bharathkumarmanickam <bharath.codefuture@gmail.com>
2020-12-25 09:18:52 +0530

    slight chagnes in frontend

--- app/src/main/java/com/example/manvaasam/login_screen.java
+++ app/src/main/java/com/example/manvaasam/login_screen.java
@@ -2,13 +2,34 @@ package com.example.manvaasam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class login_screen extends AppCompatActivity {
-
+    ImageView adminbut,courbut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        adminbut = (ImageView) findViewById(R.id.adminlogbut);
+        courbut = (ImageView) findViewById(R.id.courierlogbut);
+        adminbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login_screen.this,signin.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        courbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login_screen.this,signin.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
