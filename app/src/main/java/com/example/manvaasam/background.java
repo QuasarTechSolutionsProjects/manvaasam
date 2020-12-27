2ff6d45741c222af2acdea15978e049f5830fc55
bharathkumarmanickam <bharath.codefuture@gmail.com>
2020-12-27 20:44:40 +0530

    create package and main page created

---
+++ app/src/main/java/com/example/manvaasam/background.java
@@ -0,0 +1,129 @@
+package com.example.manvaasam;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+
+public class background extends AsyncTask <String, Void,String> {
+    AlertDialog dialog;
+    Context context;
+    ProgressDialog progressDialog;
+    String msg;
+    public Boolean login = false;
+    public background(Context context)
+    {
+        this.context = context;
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+        progressDialog = ProgressDialog.show(context,
+                "Validating....", null);
+        dialog = new AlertDialog.Builder(context).create();
+        dialog.setTitle("Welcome To Manvaasam");
+
+    }
+    @Override
+    protected void onPostExecute(String s) {
+
+        if(s.contains("successfully login"))
+        {
+            dialog.setMessage(s);
+            dialog.show();
+            Intent intent_name = new Intent();
+            intent_name.setClass(context.getApplicationContext(),MainActivity.class);
+            context.startActivity(intent_name);
+            ((Activity) context).finish();
+
+
+        }
+        else if(s.contains("Username and Password Incorrect")){
+            dialog.setMessage("Username and Password Incorrect");
+            dialog.show();
+
+                  }
+
+        else{
+//            dialog.setMessage(s);
+           dialog.setMessage("Error in Connection please Try after some time Thank you");
+           dialog.show();
+        }
+    }
+
+
+
+    @Override
+    protected String doInBackground(String... voids) {
+        if(progressDialog.isShowing()){
+            progressDialog.dismiss();
+        }
+        msg = "Please wait logging in";
+        publishProgress(msg);
+        String result = "";
+        String user = voids[0];
+        String pass = voids[1];
+        String choice = voids[2];
+
+        String connstr = "http://192.168.29.180:80/manvaasam/login.php";
+
+        try {
+            URL url = new URL(connstr);
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod("POST");
+            http.setDoInput(true);
+            http.setDoOutput(true);
+            http.setDoOutput(true);
+
+            OutputStream ops = http.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(ops,"UTF-8"));
+            String data = URLEncoder.encode("username","UTF-8")+"="+URLEncoder.encode(user,"UTF-8")
+                    +"&&"+URLEncoder.encode("userpas","UTF-8")+"="+URLEncoder.encode(pass,"UTF-8")
+                    +"&&"+URLEncoder.encode("tablevalue","UTF-8")+"="+URLEncoder.encode(choice,"UTF-8");
+            writer.write(data);
+            writer.flush();
+            writer.close();
+            ops.close();
+
+            InputStream ips = http.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(ips,"ISO-8859-1"));
+            String line ="";
+            while ((line = reader.readLine()) != null)
+            {
+                result += line;
+            }
+            reader.close();
+            ips.close();
+            http.disconnect();
+            return result;
+
+        } catch (MalformedURLException e) {
+            result = e.getMessage();
+        } catch (IOException e) {
+            result = e.getMessage();
+        }
+
+
+        return result;
+    }
+
+    private void publishProgress(String msg) {
+        progressDialog.setMessage(msg);
+    }
+    }
+
