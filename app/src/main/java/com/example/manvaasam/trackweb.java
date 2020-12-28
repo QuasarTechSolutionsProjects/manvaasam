3f86c2d444aecd5e688ab7f11fa610417ee0855c
bharathkumarmanickam <bharath.codefuture@gmail.com>
2020-12-28 11:41:39 +0530

    additional changes done

---
+++ app/src/main/java/com/example/manvaasam/trackweb.java
@@ -0,0 +1,14 @@
+package com.example.manvaasam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trackweb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trackweb);
+    }
+}
