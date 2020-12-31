ee441649e6dc527667bbe9de665ed2367ec5eb90
Mati02K <mati02official@gmail.com>
2020-12-31 09:53:55 +0530

    Search Option done check and tell

--- app/src/main/java/com/example/manvaasam/searchact.java
+++ app/src/main/java/com/example/manvaasam/searchact.java
@@ -11,15 +11,21 @@ import com.google.android.material.textfield.TextInputEditText;
 public class searchact extends AppCompatActivity {
     FloatingActionButton flobar;
     TextInputEditText tx1;
+    String mid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchact);
         tx1=(TextInputEditText) findViewById(R.id.sid);
+        mid=tx1.getText();
         flobar = (FloatingActionButton) findViewById(R.id.fbar);
         flobar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent i = new Intent(searchact.this, searchpackt.class);
+                i.putExtra("M_id",mid);
+                Intent intent = new Intent(searchact.this,searchpackt.class);
+                startActivity(intent);
 
             }
         });
