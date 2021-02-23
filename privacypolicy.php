0a08128a321202b90c6ef8d5f527bc253dc09e0f
Mati02K <mati02official@gmail.com>
2021-02-23 13:01:40 +0530

    Privacy Policy Added

---
+++ privacypolicy.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Manvaasam Logistics</title>
+  <meta content="Logistics Admn Panel for Manvaasam Logistics APP" name="description">
+  <meta content="Manvaasam, Manvaasam Logistics, App" name="keywords">
+
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/Favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/Favicon//favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/Favicon//favicon-16x16.png">
+  <link rel="manifest" href="assets/img/Favicon//site.webmanifest">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+
+  <!-- Google Fonts -->
+
+  
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <!-- Vendor CSS Files -->
+  
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style2.css" rel="stylesheet">
+</head>
+
+<body>
+  
+    <a href="https://manvaasam.com/"> <img src="logo.jpeg" style="max-height:100px;max-width:100px;" alt="Manvaasam">
+    </a>
+  
+
+  <div class="container-term100">
+    <div class="wrap-term100">
+        <p class="login100-title" style="text-align: center">
+         Manvaasam Logistics Privacy Policy
+        </p>
+        
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 col-md-offset-2">
+				<div class="panel panel-default" style="margin-top:20px;">
+					<div class="panel-body" style="max-height:600px; overflow-y:scroll; background-color:white;">
+						<div style="text-align:left;">
+						    <p> 1. Manvaasam Logistics Application is owned by Manvaasam Team and is a Application <b> used for Internal Logistics Transactions only</b>.</p>
+                            <p> 2. The Users of the app are Manvaasam Team Members and St Courier Logistics Company who reside in India.</p>
+                            <p>3.  We only collect the data regarding the package details and <b> use the Mobile's camera for Scanning the Package ID only </b>.</p>
+                            <p>4.  We collect these data <b> solely to run our Logistics Business</b> (i.e send a Package from one place to another)</p>
+                            <p>5.  We <b> do not provide or share any user's data with a third party</b>.</p>
+                            <p>6.  Users have the right to view the data stored and limit the app's permission.</p>
+                            <p>7.  We will notify all our Workers / Users if there is any change in Privacy Policy.</p>
+                            <p>8.  <b>Date of Last Revision: 21/02/2021</b>.</p>
+                        
+						</div>
+					</div>
+				</div>
+		</div>
+	</div>
+</div>
+
+    </div>
+  </div>
+
+</body>
+
+</html>
