1451d2da9c799970f384eea4bd6fc7d368f29946
Mati02K <mati02official@gmail.com>
2021-08-12 00:25:06 +0530

    Total Amount of userid is changed to Total Packages

--- mtotamt.php
+++ mtotamt.php
@@ -2,27 +2,26 @@
 header('Content-Type: application/json');
   require("config.php");
   include_once("shaping.php");
-$totalamt=0;
+
 if(isset($_GET['logid']))
 {
     $logid = cryptfun('encrypt',$_GET['logid']);
  
-    $sql = "SELECT p_amt FROM package  WHERE c_uname = '$logid'";
+    $sql = "SELECT COUNT('man_id') AS total_packages FROM package  WHERE c_uname = '$logid'";
 
     $res = mysqli_query($conn,$sql);
 
     
     $packages = array();
     
-    while($row = mysqli_fetch_array($res))
-    {
-    $rowamount=cryptfun('decrypt',$row['p_amt']);
-    $totalamt=$totalamt+$rowamount;
-    }
+    $row = mysqli_fetch_assoc($res);
+
+    $totalpackages = $row['total_packages'];
     
     array_push($packages,array('logid'=>$logid,
-    'totamt'=>$totalamt)
+    'totpack'=>$totalpackages)
     );
+    
     echo json_encode(array("packages"=>$packages));
   
 
